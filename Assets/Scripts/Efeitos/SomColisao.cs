177faa99d131b29af97f13b5d6a12a9673d631d0
Renan <renandreschmartins@gmail.com>
2020-11-01 18:44:03 -0300

    Sons

---
+++ Assets/Scripts/Efeitos/SomColisao.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SomColisao : MonoBehaviour
+{
+    public AudioSource fonteSonora;
+    private bool isTrigger;
+
+    private void Start()
+    {
+        isTrigger = GetComponent<Collider>().isTrigger;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!isTrigger)
+        {
+            fonteSonora.pitch = Random.Range(1f, 2f);
+            fonteSonora.PlayOneShot(fonteSonora.clip);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isTrigger)
+        {
+            fonteSonora.pitch = Random.Range(1f, 2f);
+            fonteSonora.PlayOneShot(fonteSonora.clip);
+        }
+    }
+}
